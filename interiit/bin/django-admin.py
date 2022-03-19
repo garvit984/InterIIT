7a34f539712eb229f5cf4bbb8d1dc45cae0ecf21
garvit984 <garvitagarwal984@gmail.com>
2022-03-19 16:03:17 +0530

    Initial Commit

---
+++ interiit/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/this/Downloads/Garbage_Truck_Monitoring_System-master/interiit/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
